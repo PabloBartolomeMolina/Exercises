6f19a7ba28556034b7e571030925cc009d63668b
Pablo Bartolomé <pablobart.me@gmail.com>
2021-11-10 08:15:23 +0100

    Simplification of the squared_root function. Now, it works faster thrrough estimations, giving decimals results

--- myMath.py
+++ myMath.py
@@ -34,62 +34,24 @@ def powered(base, index):
         result = base
         for x in range(1, index):
             result = result * base
-    print("Power is ", result)
+    #print("Power is ", result)
     return result
 
 
-def squared_root(base):
-    list_digits = list()
-    result = 1
-    rest = 0
-    flag_continue = 0
+'''
+Input : Number to calculate the squared root from.
+Margin : Precision for the result of the squared root calculation
+'''
+def squared_root(input, margin):
+    estimated = 1.0
 
-    # Get number of digits of number to calculate squared root from.
-    digits = int(math.log10(base)) + 1
-    print("Number of digits", digits)
-    # Separate digits into a list to properly calculate the squared root.
-    if (digits % 2) == 0: # Par, even
-        ''' Debugging purposes
-        print("odd")
-        print("", (digits % 2))
-        print("", (digits / 2)+1)
+    while abs(powered(estimated, 2) - input) >= margin:
+        result = input / estimated
+        med = (result + estimated) / 2.0;
+        estimated = med
+
         '''
-        if digits == 2:
-            list_digits.append(base)
-        else:
-            for i in range(0, digits, 2):
-                list_digits.append(str(base)[i:i+2])
-
-    else:               # Impar, odd
-        print("even")
-        list_digits.append(str(base)[0])  # First digit
-        for i in range(1, digits, 2):
-            list_digits.append(int(str(base)[i:i + 2]))
-        pass
-
-    print(list_digits)
-
-    for i in range(len(list_digits)):
-        for guess in range(1, base):
-            computed = int(list_digits[i]) / powered(guess, 2)
-
-            if computed > 1:
-                # Squared root should be bigger
-                pass
-            elif computed == 1:
-                # Exact result
-                if i == 0:
-                    result = guess
-                else:
-                    result = (result*10) + guess
-                break
-            else:
-                if i == 0:
-                    result = (guess - 1)
-                else:
-                    result = (result * 10) + (guess - 1)
-                break
-
-        print("Squared root is", result)
-        print("Number to calculate squared root from is", base)
-    return result
+        #Debugging purposes
+        print("Estimation :", estimated)
+        print("SQRT :", result)'''
+    return estimated
