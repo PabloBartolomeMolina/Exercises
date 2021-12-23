5565cff675b0b4984ba5311f21f502d19e228e82
Pablo Bartolomé <pablobart.me@gmail.com>
2021-12-23 20:24:46 +0100

    New file to modify and treat files with bytes information

---
+++ bytes_management.py
@@ -0,0 +1,35 @@
+# Functions to treat files with information in hex format
+
+import os.path
+
+
+def check_csv_format(path):
+    # Check if it is possible to open the file.
+    try:
+        f = open(path, 'r')
+        print("yay")
+        flag_open = True
+    except IOError:
+        print("Oops, couldn't open the file")
+        flag_open = False
+
+    # Check if already in CSV format
+    csv = False
+    datafile = f.readlines()
+    for line in datafile:
+        if "," in line:
+            csv = True
+            break
+    # Print result, mainly for debugging purposes.
+    print ("CSV format ? ", csv)
+
+    if flag_open:
+        f.close()   # Close file if open
+    else:
+        pass        # File is not open
+
+    return csv
+
+
+def set_csv_format(path):
+    pass
