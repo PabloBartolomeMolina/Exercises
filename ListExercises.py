48de8466d79436e7f071aefe393623aa2b56abcb
Pablo Bartolomé <pablobart.me@gmail.com>
2022-01-15 21:45:11 +0100

    Reformat debug outputs to make results more readable : Identify the matrices that are printed

--- ListExercises.py
+++ ListExercises.py
@@ -3,15 +3,15 @@
 
 # Generate a 3d List, with the size indicated through the parameters
 def generate_3D_list(x: int, y: int, z: int):
-    #new_list = [[['*' for col in range(x)] for col in range(y)] for col in range(z)]
-
     # 1D list case
     dimA = ['*' for col in range(x)]
+    print('dimA ', dimA)
     # 2D list case
     if x == 0:
         dimB = ['*' for col in range(y)]    # 1D list
     else:
         dimB = [dimA for col in range(y)]    # 2D list
+    print('dimB ', dimB)
     # 3D list case
     if x == 0 and y == 0:
         new_list = ['*' for col in range(z)]    # 1D list
@@ -21,4 +21,4 @@ def generate_3D_list(x: int, y: int, z: int):
         new_list = [dimA for col in range(z)]    # 2D list
     else:
         new_list = [dimB for col in range(z)]    # 3D list
-    print(new_list)
+    print('new_list ', new_list)
