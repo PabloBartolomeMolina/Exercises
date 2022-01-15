40eda75bdad700bd8b516c8fc3d5af56743da3bc
Pablo Bartolomé <pablobart.me@gmail.com>
2022-01-15 21:34:26 +0100

    Create 3 lists, each with one dimension more than the precedent one

--- ListExercises.py
+++ ListExercises.py
@@ -3,5 +3,12 @@
 
 # Generate a 3d List, with the size indicated through the parameters
 def generate_3D_list(x: int, y: int, z: int):
-    new_list = [[['*' for col in range(x)] for col in range(y)] for col in range(z)]
+    #new_list = [[['*' for col in range(x)] for col in range(y)] for col in range(z)]
+
+    # 1D list
+    dimA = ['*' for col in range(x)]
+    # 2D list
+    dimB = [dimA for col in range(y)]
+    # 3D list
+    new_list = [dimB for col in range(z)]
     print(new_list)
