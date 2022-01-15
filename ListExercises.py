f442805637947b5cc3527d09e2327b91fd7b4a79
Pablo Bartolomé <pablobart.me@gmail.com>
2022-01-15 21:38:17 +0100

    Create 3 lists, each with one dimension more than the precedent one. Take into account null dimensions to properly create the lists

--- ListExercises.py
+++ ListExercises.py
@@ -5,10 +5,20 @@
 def generate_3D_list(x: int, y: int, z: int):
     #new_list = [[['*' for col in range(x)] for col in range(y)] for col in range(z)]
 
-    # 1D list
+    # 1D list case
     dimA = ['*' for col in range(x)]
-    # 2D list
-    dimB = [dimA for col in range(y)]
-    # 3D list
-    new_list = [dimB for col in range(z)]
+    # 2D list case
+    if x == 0:
+        dimB = ['*' for col in range(y)]    # 1D list
+    else:
+        dimB = [dimA for col in range(y)]    # 2D list
+    # 3D list case
+    if x == 0 and y == 0:
+        new_list = ['*' for col in range(z)]    # 1D list
+    elif x == 0:
+        new_list = [dimB for col in range(z)]    # 2D list
+    elif y == 0:
+        new_list = [dimA for col in range(z)]    # 2D list
+    else:
+        new_list = [dimB for col in range(z)]    # 3D list
     print(new_list)
