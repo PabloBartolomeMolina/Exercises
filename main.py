de6c6e34f5df1e1d4d89efe47848c68dc07a1434
Pablo Bartolomé <pablobart.me@gmail.com>
2022-01-15 21:44:33 +0100

    Reformat debug outputs to make results more readable

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ import ListExercises as le
 if __name__ == '__main__':
     path = "bytes_raw.txt"
 
+    print("Test case 1:")
     le.generate_3D_list(3, 6, 4)
+    print("\n")
+    print("Test case 2:")
     le.generate_3D_list(0, 6, 4)
+    print("\n")
+    print("Test case 3:")
     le.generate_3D_list(3, 0, 4)
+    print("\n")
+    print("Test case 4:")
     le.generate_3D_list(0, 0, 4)
+    print("\n")
+    print("Test case 5:")
     le.generate_3D_list(3, 6, 0)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
