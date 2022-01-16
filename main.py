a0c2d646def9d082881670fea27ec5d7b5dff72d
Pablo Bartolomé <pablobart.me@gmail.com>
2022-01-16 21:19:08 +0100

    Vector tests for printing the differences between two given lists

--- main.py
+++ main.py
@@ -17,17 +17,22 @@ if __name__ == '__main__':
 
     print("Test case 1:")
     le.compare_lists([1, 2, 3], [1, 2, 3])
+    le.different_lists([1, 2, 3], [1, 2, 3])
     print("\n"),
     print("Test case 2:")
     le.compare_lists([1, 2, 3, 4], [1, 2, 3])
+    le.different_lists([1, 2, 3, 4], [1, 2, 3])
     print("\n")
     print("Test case 3:")
     le.compare_lists([1, 2, 3], [1, 2, 3, 4])
+    le.different_lists([1, 2, 3], [1, 2, 3, 4])
     print("\n")
     print("Test case 4:")
     le.compare_lists([1, 3, 3], [1, 2, 3])
+    le.different_lists([1, 3, 3], [1, 2, 3])
     print("\n")
     print("Test case 5:")
     le.compare_lists([1, 2, 3], [1, 3, 3])
+    le.different_lists([1, 2, 3], [1, 3, 3])
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
